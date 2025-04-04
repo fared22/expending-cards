adc066f3bcab396f435aacd6a57d615d71c05d11
fared22 <faredtony4@gmail.com>
2025-04-04 17:49:48 +0200

    test

---
+++ main.js
@@ -0,0 +1,11 @@
+let panals = document.getElementsByClassName("panal");
+
+for (let i = 0; i < panals.length; i++) {
+  panals[i].addEventListener("click", function () {
+    let panal = document.querySelector(".panal.active");
+    if (panal) {
+      panal.classList.remove("active");
+    }
+    this.classList.add("active");
+  });
+}
